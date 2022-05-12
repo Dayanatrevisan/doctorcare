4465a3140eb4906baef1b9720a36784ffdb49cea
Dayanatrevisan <105463367+Dayanatrevisan@users.noreply.github.com>
2022-05-12 18:15:20 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,89 @@
+//string (textos)
+//number (número)
+//boolean (true/false)
+
+window.addEventListener('scroll', onScroll)
+onScroll()
+
+//gerencia as funcoes de scroll da pagina
+function onScroll() {
+  showNavOnScroll()
+  showBackToToButtonOnScroll()
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+
+ function activateMenuAtCurrentSection(section){
+
+ 
+ const targetLine = scrollY + innerHeight / 2;
+
+
+ const sectionTop = section.offsetTop
+ const sectionHeight = section.offsetHeight
+ const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+
+ const sectionEndAt = sectionTop + sectionHeight
+ const sectionEndPassedTargetline = sectionEndAt <= targetLine
+
+
+
+ const sectionBoundaries = sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+ const sectionId = section.getAttribute('id')
+
+ const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+
+ menuElement.classList.remove('active')
+ if (sectionBoundaries) {
+   menuElement.classList.add('active')
+ }
+
+}
+
+
+
+
+
+
+ 
+
+//mostrar o navigation ao fazer scroll
+function showNavOnScroll(){ 
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+//mostraar botao back to top ao fazer scroll
+function showBackToToButtonOnScroll(){
+  
+  if (scrollY > 500) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+
+ScrollReveal({
+  origin: 'top',
+  distance: '3rem',
+  duration: 700,
+
+}).reveal('#home, #home img, #home .stats, #services, #services header, #services .card, #about, #about header, #about .content');
+
